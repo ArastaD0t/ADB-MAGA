322f742f6b40df2c0bd6040a1c8ea6022c057d39
ArastaD0t <38872983+ArastaD0t@users.noreply.github.com>
2022-04-26 19:56:03 +0000

    Add files via upload

---
+++ ControllConducted.sh
@@ -0,0 +1,12 @@
+alias Button='./adbTouch.sh'
+alies RoundButton='./adbTouchRadio.sh'
+alias Text='adb shell input text '
+alias 
+
+
+
+
+Button 'Nastavení'
+Text 'Muah'
+
+
--- fgmail.sh
+++ fgmail.sh
@@ -1,1 +1,1 @@
-cat /tmp/view.xml|egrep -o "[^[:space:]]+@[^[:space:]]+" | tr -d "<>"| cut -d '=' -f 2 | sed 's/"//g'|grep -m1 "" 
+cat /tmp/view.xml|egrep -o "[^[:space:]]+@[^[:space:]]+" | tr -d "<>"| cut -d '=' -f 2 | sed 's/"//g'|grep -m1 ""  > /tmp/gmail.var
